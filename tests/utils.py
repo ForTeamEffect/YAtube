b621b2a2391cb7c4a3459ba71c36933e5f7c97a8
Mak Main <diashov@yandex.ru>
2023-08-16 02:41:24 -0300

    na reviu

--- tests/utils.py
+++ tests/utils.py
@@ -189,3 +189,7 @@ def get_field_from_context(context, field_type):
         if field not in ('user', 'request') and isinstance(context[field], field_type):
             return context[field]
     return
+
+
+if __name__ != '__main__':
+    pass

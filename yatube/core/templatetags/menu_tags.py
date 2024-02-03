72ada8017c615652f363bab8b132e1b3e675a3b5
Mak Main <diashov@yandex.ru>
2024-02-03 08:32:28 +0000

    new_3

--- yatube/core/templatetags/menu_tags.py
+++ yatube/core/templatetags/menu_tags.py
@@ -1,33 +1,35 @@
-
 from menu.models import MenuItem
-
 from django import template
-from django.urls import resolve
 
 register = template.Library()
 
 
 @register.inclusion_tag('menu.html')
 def draw_menu(menu_name, request):
-    menu_items = MenuItem.objects.select_related('parent')
-    active_url = 'http://127.0.0.1:8000'+request.path
-    print(active_url)
-    active_item = None
-    ancestors = list()
+    menu_items = MenuItem.objects.select_related('parent')  # выбираем все элементы меню с их родительскими элементами
+    active_url = 'http://127.0.0.1:8000' + request.path  # формируем активный URL из запроса
+    print(active_url)  # выводим активный URL в консоль (для отладки)
+    active_item = None  # инициализируем активный элемент как None
+    ancestors = list()  # создаем пустой список для предков
 
+    # проходимся по всем элементам меню
     for item in menu_items:
-        print(item)
-        if item.url == active_url:
-            active_item = item
-            current_item = active_item
-            while current_item:
-                ancestors.append(current_item)
-                current_item = current_item.parent
-            break
-    reverse_ancestors = []
-    i = -1
-    for item in range(len(ancestors[1:])):
-        reverse_ancestors.append(ancestors[i])
-        i -= 1
-    print({'menu_items': menu_items, 'active_item': active_item, 'ancestors': reverse_ancestors})
-    return {'menu_items': menu_items, 'active_item': active_item, 'ancestors': reverse_ancestors, 'enumerat':enumerate(ancestors)}
+        print(item)  # выводим каждый элемент меню в консоль (для отладки)
+        if item.url == active_url:  # если URL элемента совпадает с активным URL
+            active_item = item  # устанавливаем текущий элемент как активный
+            current_item = active_item  # устанавливаем текущий элемент как текущий активный элемент
+            while current_item:  # продолжаем цикл, пока текущий элемент существует
+                ancestors.append(current_item)  # добавляем текущий элемент в список предков
+                current_item = current_item.parent  # устанавливаем текущим элементом его родителя
+            break  # выходим из цикла
+
+    # не работал reverse()
+
+    reverse_ancestors = []  # создаем пустой список для реверса предков
+    i = -1  # начинаем с последнего элемента в списке
+    for item in range(len(ancestors[1:])):  # проходим по всем элементам предков, начиная с первого
+        reverse_ancestors.append(ancestors[i])  # добавляем элемент в список реверса
+        i -= 1  # уменьшаем индекс для перехода к предыдущему элементу
+
+    # возвращаем словарь с данными для шаблона
+    return {'menu_items': menu_items, 'active_item': active_item, 'ancestors': reverse_ancestors}
